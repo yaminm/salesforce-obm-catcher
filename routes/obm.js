7796ae20fa82ab6928bc54eaea58953a841f8487
myamin <yamin.menash@gmail.com>
2018-11-05 13:52:22 +0200

    added orgId,LocalSystem and evnURL'

--- routes/obm.js
+++ routes/obm.js
@@ -6,7 +6,10 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var recordSchema = mongoose.Schema({
   Id: { type: String },
-  FeildsValue: { type: String }
+  FeildsValue: { type: String },
+  orgId: {type: String},
+  localSystem: {type: String},
+  envURL: {type:String}
 })
 var recordModel = mongoose.model( 'record', recordSchema,'Records');
 
@@ -16,20 +19,21 @@ router.post('/', function(req, res) {
   // get the obm as an object
   console.log('IN POST!');
   console.log('message :: ',JSON.stringify(req.body));
+  var localSystem = req.param('localSystem');
   var message = unwrapMessage(req.body);
 
-  console.log('record1');
   console.log('recordId:' + message.recordId);
-  console.log('recordFeildsValues:' +message.recordFeildsValues );
-  var record1 = new recordModel({Id:message.recordId,FeildsValue:JSON.stringify(message.recordFeildsValues)});
-  console.log('record1',record1);
+  var record1 = new recordModel({Id:message.recordId,
+                                 FeildsValue:JSON.stringify(message.recordFeildsValues),
+                                 orgId:message.orgId,
+                                 localSystem:localSystem,
+                                 envURL:envURL});
     // save model to database
     record1.save(function (err, record1) {
       if (err) return console.error(err);
       console.log(record1.Id + " saved to bookstore collection.");
     });
   
-  console.log('message :: ',message);
   if (!_.isEmpty(message)) {
     // some something #awesome with message
     console.log('message not empty!');
@@ -51,24 +55,19 @@ router.post('/', function(req, res) {
 // unwrap the xml and return object
 unwrapMessage = function(obj) {
   try {
-    console.log('1');
     var orgId = obj['soapenv:envelope']['soapenv:body'][0].notifications[0].organizationid[0];
-    console.log('2');
+    var envURL = obj['soapenv:envelope']['soapenv:body'][0].notifications[0].enterpriseurl[0];
     var recordId = obj['soapenv:envelope']['soapenv:body'][0].notifications[0].notification[0].sobject[0]['sf:id'][0];
-    console.log('3');
     var o = obj['soapenv:envelope']['soapenv:body'][0].notifications[0].notification[0].sobject[0];
-    console.log('4');
 
     var recordFeildsValues = Object.keys(o).
       reduce(function (res, k) { res[k] = o[k][0]; return res; }, {});
-      console.log('5');
-      console.log('recordFeildsValues:\n' + recordFeildsValues);
-
 
     return {
       orgId: orgId,
       recordId: recordId,
-      recordFeildsValues: recordFeildsValues
+      recordFeildsValues: recordFeildsValues,
+      envURL:envURL
     };
 
   } catch (e) {
