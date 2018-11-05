eaa95d14fca69fb4362a795973340498b1483238
myamin <yamin.menash@gmail.com>
2018-11-05 11:05:41 +0200

    g'

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ var db = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('connected', function () {  
+    console.log('Mongoose default connection open to ' + dbURI);
+  }); 
 
 
 
